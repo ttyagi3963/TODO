aa307eda779b75887a40ffd1f77e59cab01cb338
Tarun Tyagi <taruntyagi@taruns-mac.local>
2018-12-27 13:44:43 -0800

    Todolist complete

---
+++ TODO/TodoListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  TODO
+//
+//  Created by Tarun Tyagi on 12/26/18.
+//  Copyright © 2018 Tarun Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["find mike", "Kiss Hind", "Love Hind"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+       
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+            {
+                tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+   
+    
+
+}
+
